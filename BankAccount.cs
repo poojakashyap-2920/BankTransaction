286277337bc0c29f67b2414caffe764461d5232d
poojakashyap-2920 <pooja337026@gmail.com>
2024-03-05 17:03:09 +0530

    transaction file added

---
+++ BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDetails_Review
+{
+    class BankAccount
+    {
+        private float balance;
+
+    public BankAccount(float initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public void Deposit(float amount)
+    {
+        balance = balance + amount;
+        Console.WriteLine($"Deposited: {amount}");
+    }
+
+    public void Withdraw(float amount)
+    {
+        if (balance >= amount)
+        {
+            balance = balance - amount;
+            Console.WriteLine($"Withdrawn: {amount}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient balance");
+        }
+    }
+
+    public void CheckBalance()
+    {
+        Console.WriteLine($"Balance: {balance}");
+    }
+}
+}
