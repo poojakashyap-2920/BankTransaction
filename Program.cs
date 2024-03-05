49936fb54bb110ea9dc890a491d8ff1df13716d2
poojakashyap-2920 <pooja337026@gmail.com>
2024-03-05 17:01:26 +0530

    program.cs file added

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BankDetails_Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter initial balance:");
+            float initialBalance = float.Parse(Console.ReadLine());
+            BankAccount account = new BankAccount(initialBalance);
+
+            while (true)
+            {
+                Console.WriteLine("1. Withdraw\n2. Deposit\n3. Check Balance\n4. Exit");
+                Console.WriteLine("Enter option:");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter amount to withdraw:");
+                        float withdrawAmount = float.Parse(Console.ReadLine());
+                        account.Withdraw(withdrawAmount);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter amount to deposit:");
+                        float depositAmount =  float.Parse(Console.ReadLine());
+                        account.Deposit(depositAmount);
+                        break;
+
+                    case 3:
+                        account.CheckBalance();
+                        break;
+
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
+        }
+    }
+}
